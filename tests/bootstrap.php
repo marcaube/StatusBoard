a6dd4e319a684db35de816362ac0877a00f33998
marcaube <marc@tourismecie.com>
2014-09-28 10:51:29 -0400

    Update test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,9 @@
 <?php
 
-if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
-    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
+if (!file_exists($autoloadFile = __DIR__ . '/../vendor/autoload.php')) {
+    die('You must set up the project dependencies, run the following commands:' . PHP_EOL .
+        'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
+        'php composer.phar install --dev' . PHP_EOL);
 }
 
 require $autoloadFile;
